f6d1015ec4650850e4f4df8a0d16c917c8b5e081
mrjoelee <90262428+mrjoelee@users.noreply.github.com>
2022-01-03 00:15:05 -0500

    added and updated migration lender and itemsname

--- LenderApp/Models/Item.cs
+++ LenderApp/Models/Item.cs
@@ -8,6 +8,8 @@ namespace LenderApp.Models
         [Key]
         public int Id { get; set; }
         public string Borrower { get; set; }
+        public string Lender { get; set; }
+        public string ItemName { get; set; }
 
     }
 }
